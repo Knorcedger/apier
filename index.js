abe9449df242a0fa6b9f2f45c82abe302249db98
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-20 16:33:28 +0200

    Routing works

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+var router = require('router')();
+var reqlog = require('reqlog');
+
 exports.init = function() {
-	console.log('APIer initialized');
+	reqlog.info('apier initialized!!');
+	var app = function(req, res) {
+		// console.log('inside the app!');
+		router(req, res, function() {});
+	};
+
+	return app;
 };
+
+exports.endpoint = function(url, callback) {
+	reqlog.info('setup endpoint', url);
+	// create an object to apply as this in the callback
+	// its used to be able to call the send function inside the callback
+	// without having to also pass the req and res
+	var self = this;
+	router.get(url, [test], function(req, res) {
+		var innerSelf = {
+			req: req,
+			res: res,
+			send: function(data) {
+				self.send.call(this, data);
+			}
+		};
+		reqlog.info('inside endpoint', req.url);
+		callback.call(innerSelf, req, res);
+	});
+};
+
+exports.send = function(data) {
+	reqlog.info('inside api send');
+	this.res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+	this.res.end(data);
+};
+
+function test(req, res, next) {
+	reqlog.log('inside test');
+
+	next();
+}

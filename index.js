476a9e86c423ad52ee11f8b3bdb38f5bfe1705e0
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-27 21:59:03 +0200

    Major refactoring to make it work properly

--- index.js
+++ index.js
@@ -8,42 +8,78 @@ var bodyParser = require('body-parser');
 
 var jsonParser = bodyParser.json();
 
-var middlewares = [test, jsonParser, dataParser, responseBuilder.init];
-var self = this;
+module.exports = apier;
 
-exports.init = function() {
+/**
+ * Create an apier app
+ * @method apier
+ * @return {Function} The app to use as server
+ */
+function apier() {
 	reqlog.info('apier initialized!!');
+
 	var app = function(req, res) {
-		router(req, res, function() {});
-		router.all('*', middlewares, function(req, res) {
-			reqlog.info('inside all endpoint', req.url);
+		// this si the final handler if no match found
+		router(req, res, function() {
+			reqlog.info('NOT_FOUND:', req.method + ' ' + req.url);
 			responseBuilder.error(req, res, 'NOT_FOUND');
 		});
 	};
 
-	return app;
-};
+	app.endpoint = endpoint;
 
-exports.endpoint = function(methods, url, callback) {
-	// create an object to apply as this in the callback
-	// its used to be able to call the send function inside the callback
-	// without having to also pass the req and res
+	return app;
+}
+
+// just logs the request
+router.use(function(req, res, next) {
+	reqlog.error('REQUEST:', req.method + ' ' + req.url);
+	next();
+});
+router.use(test);
+router.use(jsonParser);
+router.use(dataParser);
+router.use(responseBuilder.init);
+// when its an options request, just respond with 200
+router.options('*', function(req, res) {
+	reqlog.info('inside options');
+	res.statusCode = 200;
+	res.end();
+});
+
+/**
+ * Create an endpoint
+ * @method endpoint
+ * @param  {array}   methods  The endpoint methods e.g. ['get'. 'post']
+ * @param  {string}   url      The endpoint path e.g. '/users'
+ * @param  {Function} callback The user defined callback
+ */
+function endpoint(methods, url, callback) {
 	for (var i = 0, length = methods.length; i < length; i++) {
 		var method = methods[i].toLowerCase();
 		reqlog.info('setup endpoint', method + ' ' + url);
-		router[method](url, middlewares, function(req, res) {
+		router[method](url, function(req, res) {
 			routerCallback(req, res, callback);
 		});
 	}
-};
+}
 
-exports.send = function(data) {
+/**
+ * Just sends the response using this
+ * @method send
+ * @param  {any} data The data to return
+ */
+function send(data) {
 	reqlog.info('inside api send');
 	responseBuilder.send(this.req, this.res, data);
-};
+}
 
 /**
  * Defines the endpoint callback passed into the router
+ *
+ * create an object to apply as this in the callback
+ * its used to be able to call the send function inside the callback
+ * without having to also pass the req and res
  * @method routerCallback
  * @param  {object}       req      A request object
  * @param  {object}       res      A response object
@@ -54,7 +90,7 @@ function routerCallback(req, res, callback) {
 		req: req,
 		res: res,
 		send: function(data) {
-			self.send.call(this, data);
+			send.call(this, data);
 		}
 	};
 	reqlog.info('inside routerCallback for endpoint: ', req.url);
